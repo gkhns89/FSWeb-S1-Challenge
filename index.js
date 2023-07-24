e7193bfe1d57cb3a57576ed832dcfa10f0d87d4c
gkhns89 <gokhansisman@outlook.com>
2023-07-25 01:30:37 +0300

    Görev 3  tamamlandı.

--- index.js
+++ index.js
@@ -166,8 +166,11 @@ Aşağıdaki işlemleri yapmak için indekseGoreFenomen işlevini kullanın:
 NOT: DÖNDÜĞÜNÜZ DİZİN YUKARIDAKİ BİÇİMLE EŞLEŞMESİ GEREKİR, YA DA TESTİ GEÇMEYECEKTİR!
 ÖRNEK: fenomenler dizisi ve 3 sayısı ile indekseGoreFenomen çağrılırsa, `3. indekste bulunan fenomen: Leo Messi' */
 
-function indekseGoreFenomen(/*kod*/) {
-  /*kod*/
+function indekseGoreFenomen(fDizisi, fIndex) {
+  let fenomen =
+    fIndex + ". indekste bulunan fenomen: " + fDizisi[fIndex].profile;
+
+  return fenomen;
 }
 
 /*  Görev 4:
